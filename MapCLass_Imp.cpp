3c5e7335e8636b8f9826aaafd74b0be5b0555937
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-04-10 16:01:34 -0400

    Update MapCLass_Imp.cpp
    revised MapFunciton
    -replaced line 56 vector with map
    -added lines 79/80 to add "" around strings 
    -revised line 86 to insert pairs into map wordTokens (string,int)
    -added text references for code

--- MapCLass_Imp.cpp
+++ MapCLass_Imp.cpp
@@ -51,18 +51,20 @@ void MapClass::MapFunction(string& fileNameInput, string& rawData)
 		ifstream fileInput(fileNameInput); //retriev/open file via user input, ref pg 793 C++SL
 		cout << "\n Processing Raw Data from file: "<< fileNameInput << "\n(removing white space, puncuations, captalization, and adding, word + , 1).\n"<<"Words now have been manipulated: ";
 
-		//vector used to store processed data
-		vector<string>wordTokens;
+		//map used to store processed data **chagne to map ref 689/691 C++how to program, dietel
+		//map performs fast storage and retrieval of uniuqe keys and associated values. These values will be in order by default use of map, for unordered use unorderdmap 
+		map<string,double>wordTokens;
 	
 		//getline used to read line by line function reads all characters until the end of file is reached. while loop to go through each line o file 
 		while (getline(fileInput, rawData)) { //getline pgs 677,713,769
 			stringstream ssLines(rawData); //make and object for each line of the file
-			
+
+		
 		
 			
 			while (ssLines >> words) {
 				
-				//get total word count
+				//get total word count for all words
 				wordsCounted++;
 				//remove white space 
 				words.erase(remove_if(words.begin(), words.end(), isspace),words.end()); //remove if pg 576 C++SL, & https://stackoverflow.com/questions/83439/remove-spaces-from-stdstring-in-c 
@@ -70,29 +72,34 @@ void MapClass::MapFunction(string& fileNameInput, string& rawData)
 				//remove puncuations 
 				words.erase(remove_if(words.begin(), words.end(), ispunct), words.end());
 
-				//remove capitalizations aka lowercase all letters  ref pg 225, 564-565 C++SL, is/to functions ref pg 896 
+				//remove capitalizations aka lowercase all words  ref pg 225, 564-565 C++SL, is/to functions ref pg 896 
 				transform(words.cbegin(), words.cend(), words.begin(), [](char c) {return tolower(c); }); //formatting reference pg 686
 
-				//add + ,1 *via .insert / length() used to place position at end of word see685 C++SL & *remove \\ from line 84 to apply. Don't believe it's needed for phase 1
-				//words.insert(words.length(), " ,1" );
+				//add + ,1 *via .insert / length() used add "" in  place positions start and end of word see685 C++SL & 
+				words.insert(0, "\"");
+				words.insert(words.length(), "\"" );
+				
 				
 
-				//tokenize data via adding each word to vector 'wordTokens'
-				wordTokens.push_back(words);
+				//tokenize data via addint each word to map 'wordTokens'
+				//wordTokens.insert(words/number of occruence of words);
+				wordTokens.insert(make_pair(words, wordTokens[words]++));
 
 				//allow user to see new set of words
 				cout << words<<" "; 
 
 
 			}
-			cout << "\nManipulated words added to vector 'wordTokens' :";
-			//display vector Wordtokenes to user 
-			for (auto list : wordTokens)
-				cout <<"\n"<< list;
+			cout << "\nManipulated words added to map 'wordTokens' :\n";
+
+			//display map Wordtokenes to user 
+			for (auto wordList : wordTokens)
+				cout << "\n  (" << wordList.first << """, [" << wordList.second << "]),";
 		}
 
 		//close file after data has been utilized
 		fileInput.close();
+
 		cout << "\n\nMapfunciton completed on file: " << fileNameInput; //cout filename for user to verify
 
 		cout << "\n\nNumber of words in file:   " << wordsCounted;  // Outputting the total word count
@@ -100,7 +107,6 @@ void MapClass::MapFunction(string& fileNameInput, string& rawData)
 
 
 }
-
 
 
 //void MapClass::ExportFunction(string& fileNameOutput, string& rawData)
