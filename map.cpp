a85a611d449e84770a5c20eb66cf918116989507
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-04-12 10:14:35 -0400

    Update map.cpp
    changes in line notes to block notation

--- map.cpp
+++ map.cpp
@@ -26,6 +26,7 @@ using std::fclose;
 using std::stringstream;//ref page 805
 string lines;//variable for lines of file
 string words;//varible for words in each line
+
 // default constructor
 MapClass::MapClass()
 	
@@ -42,67 +43,63 @@ MapClass::~MapClass()
 	// nothing to do at this time
 }
 
-//Mapfunction to retrieve file and call additional function export
-//request user input for filepath to open file
+//mapFunction will retrieve user input file via cmd line
+//function will use getline and stringstream to make an object of each line of file
+//Will use a while loop made of sslines to tokenize each word and place into mapwordTokens
+//While loop from lines 66-85 will erase white space and punctions of words. Additionally insert “” around words and insert ,1 for each word occurrence. 
 void MapClass::MapFunction(string& fileNameInput, string& rawData)
 {
 		cout << "Please enter the full file path to your text file(.txt). Please use '\\\\' instead of '\\' when entering the filepath: "; 
-		cin >> fileNameInput; // User inputs file name 
-		ifstream fileInput(fileNameInput); //retriev/open file via user input, ref pg 793 C++SL
+		cin >> fileNameInput; 
+		ifstream fileInput(fileNameInput);
 		cout << "\n Processing Raw Data from file: "<< fileNameInput << "\n(removing white space, puncuations, captalization, and adding, word + , 1).\n"<<"Words now have been manipulated: ";
 
-		//map used to store processed data **chagne to map ref 689/691 C++how to program, dietel
-		//map performs fast storage and retrieval of uniuqe keys and associated values. These values will be in order by default use of map, for unordered use unorderdmap 
+		
 		map<string,double>wordTokens;
 	
-		//getline used to read line by line function reads all characters until the end of file is reached. while loop to go through each line o file 
-		while (getline(fileInput, rawData)) { //getline pgs 677,713,769
-			stringstream ssLines(rawData); //make and object for each line of the file
+		
+		while (getline(fileInput, rawData)) { 
+			stringstream ssLines(rawData); 
 
 		
 		
 			
 			while (ssLines >> words) {
 				
-				//get total word count for all words
+				
 				wordsCounted++;
-				//remove white space 
+				
 				words.erase(remove_if(words.begin(), words.end(), isspace),words.end()); //remove if pg 576 C++SL, & https://stackoverflow.com/questions/83439/remove-spaces-from-stdstring-in-c 
 				
-				//remove puncuations 
+				 
 				words.erase(remove_if(words.begin(), words.end(), ispunct), words.end());
 
-				//remove capitalizations aka lowercase all words  ref pg 225, 564-565 C++SL, is/to functions ref pg 896 
-				transform(words.cbegin(), words.cend(), words.begin(), [](char c) {return tolower(c); }); //formatting reference pg 686
-
-				//add + ,1 *via .insert / length() used add "" in  place positions start and end of word see685 C++SL & 
+				
+				transform(words.cbegin(), words.cend(), words.begin(), [](char c) {return tolower(c); });
+				
 				words.insert(0, "\"");
 				words.insert(words.length(), "\"" );
-				
-				
-
-				//tokenize data via addint each word to map 'wordTokens'
-				//wordTokens.insert(words/number of occruence of words);
+												
 				wordTokens.insert(make_pair(words, wordTokens[words]++));
 
-				//allow user to see new set of words
+				
 				cout << words<<" "; 
 
 
 			}
 			cout << "\nManipulated words added to map 'wordTokens' :\n";
 
-			//display map Wordtokenes to user 
+			
 			for (auto wordList : wordTokens)
 				cout << "\n  (" << wordList.first << """, [" << wordList.second << "]),";
 		}
 
-		//close file after data has been utilized
+		
 		fileInput.close();
 
-		cout << "\n\nMapfunciton completed on file: " << fileNameInput; //cout filename for user to verify
+		cout << "\n\nMapfunciton completed on file: " << fileNameInput; 
 
-		cout << "\n\nNumber of words in file:   " << wordsCounted;  // Outputting the total word count
+		cout << "\n\nNumber of words in file:   " << wordsCounted;  
 
 
 
