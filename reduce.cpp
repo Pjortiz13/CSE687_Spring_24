6c552b48f6648290e9655a8d3cd9732eee2aa6ea
Pedro Ortiz <148012422+Pjortiz13@users.noreply.github.com>
2024-04-14 18:58:21 +0900

    Update reduce.cpp

--- reduce.cpp
+++ reduce.cpp
@@ -8,85 +8,72 @@
     --- Project Phase 1 ---
     ----- 04/08/2024 -----
     
-    This file for the Reduce class 
-    contains function declarations that take 
-    a vector of intermediate key-value pairs 
-    from the Map class and then creates an 
-    output map with keys representing unique 
-    words and values representing their counts
+
+The reduce class consolidates the counts of unique words into a single total 
+for each and records the word along with its aggregated count to an output directory.
 */
 
 #include "reduce.h"
-#include "fileManagement.h"
-#include <iostream>
-#include <filesystem>
-#include <vector>
-namespace fs = std::filesystem;
 
-// Constructor
-Reduce::Reduce(std::string tmp, std::string out, std::string mapRes) 
-    : mTmp(tmp), mOut(out), mMapRes(mapRes) {}
-
-bool Reduce::Sort() {
-    // Handle '/' for Linux and Mac and '\'for Windows
-    fs::path mapResPath(mTmp);
-    mapResPath.append(mMapRes);
-
-    std::string filepath = mapResPath.string();
-    std::unordered_map<std::string, uint64_t> map = fileManagement::ReadMapFromFile(filepath);
-    std::unordered_map<std::string, std::vector<uint64_t>> sortRes;
-
-    for (auto it = map.begin(); it != map.end(); it++) {
-        auto key = it->first;
-        auto value = it->second;
-
-        auto sortIt = sortRes.find(key);
-        if (sortIt != sortRes.end()){
-            sortIt->second.push_back(1);
-        } else {
-            sortRes[key] = std::vector<uint64_t>(1, 1);  
-        }
-    }
-
-    fs::path sortResPath(mTmp);
-    sortResPath.append(mSortRes);
-
-    filepath = sortResPath.string();
-    return fileManagement::WriteSortMapToFile(sortRes, filepath);
+// the constructor accepts outputDir as a parameter to set the output directory
+Reduce::Reduce(std::string outputDir)
+{
+	this->outputDir = outputDir; // sets the class member outputDir to the passed argument
 }
 
-void Reduce::reduce() {
-    fs::path sortResPath(mTmp);
-    sortResPath.append(mSortRes);
+// reduce function iterates through the vector and sums up all of the occurrences
+bool Reduce::reduce(std::string word, std::vector<int> occurrences) {
 
-    std::string filepath = sortResPath.string();
-    std::unordered_map<std::string, std::vector<uint64_t>> map = fileManagement::ReadSortMapFromFile(filepath);
+    // variable to store the total count of occurrences of the word
+	int totalOccurrences = 0; 
 
-    std::unordered_map<std::string, uint64_t> reduceMap;
+	// loops through each count in the occurrences vector
+	for (auto& timesUsed : occurrences) {
+		totalOccurrences += timesUsed;
+	}
 
-    for (auto it = map.begin(); it != map.end(); it++) {
-        auto key = it->first;
-        auto value = it->second;
+    // calls in Export to write the total count to the file
+	Export(word, totalOccurrences); 
+	// return true to indicate success
+    return true; 
+}
 
-        reduceMap[key] = value.size();
-    }
+// Export function to write the key (word) and its reduced value (total occurrences) to the output file
+bool Reduce::Export(std::string key, int reducedValue)
+{
+    // convert the reduced value to string
+	std::string stringRep = std::to_string(reducedValue); 
+    // concatenate the key and its occurrences
+	std::string keyPlusOccurrences = key + " " + stringRep; 
 
-    fs::path reduceResPath(mOut);
-    reduceResPath.append(mReduceRes);
+    // fileWriter is called to write the result to the output file
+	fileWriter.write(keyPlusOccurrences); 
 
-    filepath = reduceResPath.string();
+    // return true to indicate success
+	return true; 
+}
 
-    bool res = fileManagement::WriteMapToFile(reduceMap, filepath);
+// star function to prepare files for writing the output
+void Reduce::start()
+{
+    // delete SUCCESS.txt if it exists
+	fileWriter.deleteFile(this->outputDir + "SUCCESS.txt"); 
 
-    bool statusFile;
+    // clears the contents of out.txt
+	fileWriter.clear(this->outputDir + "out.txt"); 
+     // opens out.txt for appending data
+	fileWriter.open(this->outputDir + "out.txt", std::ios::out | std::ios::app);
+}
 
-    if (res) {
-        statusFile = fileManagement::CreateEmptyFileInDir(mOut, "SUCCESS"); 
-    } else {
-        statusFile = fileManagement::CreateEmptyFileInDir(mOut, "FAILED");
-    }
+// end function called to finalizes the reduce process and marks completion
+void Reduce::end()
+{
+	// closes the currently open file 
+    fileWriter.close(); 
 
-    if (!statusFile) {
-        std::cout << "Failed to create SUCCESS/FAILED file." << std::endl;
-    }
+    // open SUCCESS.txt to indicate completion
+	fileWriter.open(this->outputDir + "SUCCESS.txt", std::ios::out); 
+
+    // closes SUCCESS.txt after marking completion
+	fileWriter.close(); 
 }
