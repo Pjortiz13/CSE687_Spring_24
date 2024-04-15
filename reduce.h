98ee38f986b4eae071a3edea7fa3a275284c9cc5
organizedanvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-04-14 20:11:54 -0400

    Create reduce.h
    Adding original reduce.h to main branch.

---
+++ reduce.h
@@ -0,0 +1,45 @@
+/*  Vimal Ramnarain
+    Group X 
+    Members:
+    Joseph Laible
+    Pedro Ortiz
+    
+    
+    CSE 687 - OOD in C++
+
+    --- Project Phase 1 ---
+    ----- 04/08/2024 -----
+    
+    This header file for the Reduce class 
+    contains function declarations that take 
+    a vector of intermediate key-value pairs 
+    from the Map class and then creates an 
+    output map with keys representing unique 
+    words and values representing their counts
+*/
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <inttypes.h>
+#include <unordered_map>
+
+class Reduce { 
+public:
+    // Constructor
+    Reduce(std::string tmp, std::string out, std::string mapRes);
+
+    // Sort
+    bool Sort();
+
+    // Reduce
+    void reduce();
+private:
+    // Any method of the class can access these members
+    std::string mTmp;
+    std::string mOut;
+    std::string mMapRes;
+    static constexpr char mSortRes[] = "sort_res.txt";
+    static constexpr char mReduceRes[] = "reduce_res.txt";
+};
