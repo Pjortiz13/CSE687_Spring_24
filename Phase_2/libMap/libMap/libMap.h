aa1cea2bdc12f2ab16810cd6724aeab6546cd733
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-05-08 13:55:34 -0400

    Update libMap.h
    comments added

--- Phase_2/libMap/libMap/libMap.h
+++ Phase_2/libMap/libMap/libMap.h
@@ -1,3 +1,19 @@
+/*
+libMap.h
+Group X
+Joseph Laible
+Pedro Ortiz
+Vimal Ramnarain 
+Professor Scott Roueche
+CSE  687 Object Oriented Design in C++
+Syracuse University 
+
+Project Phase II 
+05/08/2024
+
+The libMap.h is a header file for libMap to create a DLL to allow extern use of the map_func method.  
+*/
+
 #pragma once
 
 #ifdef LIBMAP_EXPORTS
@@ -8,5 +24,5 @@
 
 #include <string>
 #include <iostream> 
-
+//provide C linkage to DLL
 extern "C" LIBMAP_API void map_func(const char* fileName, const char* data, const char* temp);
