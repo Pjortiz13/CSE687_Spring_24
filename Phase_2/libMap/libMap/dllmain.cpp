284b2384707e0ee37d9f7bd653729b6557d28995
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-05-08 13:57:17 -0400

    Update dllmain.cpp
    add comments

--- Phase_2/libMap/libMap/dllmain.cpp
+++ Phase_2/libMap/libMap/dllmain.cpp
@@ -1,4 +1,19 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
+/*
+dllmain.cpp
+Group X
+Joseph Laible
+Pedro Ortiz
+Vimal Ramnarain 
+Professor Scott Roueche
+CSE  687 Object Oriented Design in C++
+Syracuse University 
+
+Project Phase II 
+05/08/2024
+
+dllmain.cpp : Defines the entry point for the DLL application.
+*/ 
+
 #include "framework.h"
 
 BOOL APIENTRY DllMain( HMODULE hModule,
