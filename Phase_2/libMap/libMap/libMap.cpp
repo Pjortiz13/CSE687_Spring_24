bd4b670d7bd9ccf251a1188b48b500f09ca556ed
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-05-08 13:52:08 -0400

    Update libMap.cpp
    Added comments

--- Phase_2/libMap/libMap/libMap.cpp
+++ Phase_2/libMap/libMap/libMap.cpp
@@ -1,3 +1,24 @@
+/*
+libMap.cpp
+Group X
+Joseph Laible
+Pedro Ortiz
+Vimal Ramnarain 
+Professor Scott Roueche
+CSE  687 Object Oriented Design in C++
+Syracuse University 
+
+Project Phase II 
+05/08/2024
+
+The libmap.cpp file defines the method map_func which processes text data by tokenizing input strings, 
+normalizing them,converting to lowercase and removing punctuation, and recording the 
+frequency of each word. It outputs these word counts to a specified temporary directory file, 
+supporting the mapping phase of a MapReduce workflow by preparing data for subsequent reduction.
+*/
+
+
+
 #pragma once
 #include "libMap.h"
 #include "framework.h"
@@ -11,6 +32,7 @@
 #include <string>
 #include <tuple>
 
+// provide DLL linkage and call map_func to: processes the text data, tokenizes it, converts to lowercase, removes punctuation, and exports counts to temp directory with buffering
 LIBMAP_API void map_func(const char* fileName, const char* data, const char* temp) {
 
     std::ofstream out(std::string(temp) + "/map_output.txt", std::ios::app);
@@ -34,14 +56,17 @@ LIBMAP_API void map_func(const char* fileName, const char* data, const char* tem
         // Export the word
         std::tuple<std::string, int> wordsTuple(word, 1);
         buffer.push_back(wordsTuple);
-
+        //enduce buffer fto write  to temp output file
         if (buffer.size() >= bufferSize) {
+            // write each tuple in the buffer with added syntax
             for (const auto& tuple : buffer) {
                 std::string outputString = "(\"" + std::get<0>(tuple) + "\", " + std::to_string(std::get<1>(tuple)) + ") ";
                 out << outputString << std::endl;
             }
+            // Clear the buffer 
             buffer.clear();
         }
     }
+    //close the file
     out.close();
 }
