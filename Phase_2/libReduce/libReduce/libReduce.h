b05d1640405f31d77fa37389af55e921e3dc4fb7
JoeLaible123 <165977540+JoeLaible123@users.noreply.github.com>
2024-05-08 14:06:02 -0400

    Update libReduce.h
    added comments

--- Phase_2/libReduce/libReduce/libReduce.h
+++ Phase_2/libReduce/libReduce/libReduce.h
@@ -1,3 +1,20 @@
+/*
+libReduce.h
+Group X
+Joseph Laible
+Pedro Ortiz
+Vimal Ramnarain 
+Professor Scott Roueche
+CSE  687 Object Oriented Design in C++
+Syracuse University 
+
+Project Phase II 
+05/08/2024
+
+This is the header file for the  Reduce method: for function definitions and DLL C linkage
+
+*/ 
+
 #pragma once
 
 #include <string>
@@ -7,5 +24,5 @@
 #else
 #define LIBREDUCE_API __declspec(dllimport)
 #endif
-
+//Provide DLL C linkage 
 extern "C" LIBREDUCE_API void reduce(const char* outputDir);
