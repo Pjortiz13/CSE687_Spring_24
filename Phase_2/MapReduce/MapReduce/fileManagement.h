f85c140b4ae7e463a6f3a61e4b994a7ca59fb46e
organizedanvrchy <154699780+organizedanvrchy@users.noreply.github.com>
2024-05-08 16:28:35 -0400

    Updated fileManagement.h

--- Phase_2/MapReduce/MapReduce/fileManagement.h
+++ Phase_2/MapReduce/MapReduce/fileManagement.h
@@ -31,25 +31,24 @@ class fileManagement {
 public:
    // initializes the fileManagement object
     fileManagement();
-  // closes any open file streams upon destruction
+   // closes any open file streams upon destruction
     ~fileManagement();
 
-  // opens a file with given mode
+   // opens a file with given mode
     bool openFile(const std::string& filename, std::ios_base::openmode mode);
- // writes data followed by a newline to the currently open file
+   // writes data followed by a newline to the currently open file
     void writeFile(const std::string& data);
    // reads the next line or data block from the open file
     bool readNextBlock(std::string& data);
-    // closes the currently open file
+   // closes the currently open file
     void closeFile();
-// clears the contents of the specified file
+   // clears the contents of the specified file
     void clearFile(const std::string& filename);
-  // deletes the specified file
+   // deletes the specified file
     void removeFile(const std::string& filename);
- // checks if a specified directory exists
+   // checks if a specified directory exists
     static bool directoryExists(const std::string& dirPath);
-
-    // creates an empty file in the specified directory
+   // creates an empty file in the specified directory
     static bool CreateEmptyFileInDir(const std::string& dirPath, const std::string& fileName);
 
 private:
